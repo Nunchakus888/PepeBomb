a37e1816f4c15ee204205172199af03f8826ba1e
roidder <1023268613@qq.com>
2024-04-22 03:02:44 +0800

    1/3

---
+++ src/components/f2h/static.js
@@ -0,0 +1,22 @@
+export default {
+  title: 'PepeBomb',
+  icon: '',
+  description: 'PepeBomb',
+  header: {
+    title: 'PepeBomb',
+  },
+  overview: {
+    bg: '',
+    header: 'Cryptocurrency Introduction',
+    items: [
+      {
+        title: 'Project Overview',
+        description: 'PepeBomb is an innovative digital currency project issued on the Ethereum blockchain as an ERC-20 standard token. This project leverages the technological advantages of cryptocurrencies, combined with the popular culture icon of Pepe the Frog, to create a new medium for investment and trading. PepeBomb aims to attract a wide range of investors\' attention, especially among the younger generation of cryptocurrency enthusiasts, through its unique market positioning.'
+      },
+      {
+        title: 'Technical Implementation',
+        description: 'PepeBomb utilizes the Ethereum platform, ensuring compatibility with widely-used DeFi applications and services. Ethereum\'s smart contract capabilities enable PepeBomb to be more than just a trading tool, with a rich potential for applications including but not limited to:'
+      }
+    ]
+  }
+}
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,35 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      textShadow: {
+        title: '0px 2px 1px rgba(0, 0, 0, 0.25)',
+      },
+      boxShadow: {
+        header: '0px 4px 4px rgba(0, 0, 0, 0.15000000596046448)',
+      },
+      backgroundColor: {
+        bomb: '#4C9540',
+      },
+      fontFamily: {
+        bomb: ['Raleway'],
+        roboto: ['Roboto'],
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    plugin(function ({matchUtilities, theme}) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        {values: theme('textShadow')}
+      )
+    }),
+  ],
 };

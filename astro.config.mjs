d4e84d868f36c869d151d6bbc8d3c9eac76407dc
roidder <1023268613@qq.com>
2024-04-22 07:36:24 +0800

    ???

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import postcss from 'postcss';
-import pxtorem from 'postcss-pxtorem';
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [tailwind()],
-  vite: {
-    plugins: [
-      postcss({
-        plugins: [
-          pxtorem({
-            rootValue: 16, // Conversion rate, default 16
-            propList: ['*'], // Properties to convert, default: ['*']
-            minPixelValue: 2 // Minimum pixel value to convert, default: 2
-          })
-        ]
-      })
-    ]
-  }
 });

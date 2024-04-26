6cc0b1f8086ea49adbcc8c69874523397e9aee25
roidder <1023268613@qq.com>
2024-04-26 15:44:08 +0800

    vercel deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   adapter: vercel(),
-  output: 'static',
+  output: 'server',
   integrations: [tailwind()],
 });
